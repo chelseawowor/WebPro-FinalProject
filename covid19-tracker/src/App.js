154ec15e9e7138e6ead57d4a2f522402a407fd07
chelseawowor <s21810201@student.unklab.ac.id>
2020-12-06 16:00:40 +0800

    second commit

---
+++ covid19-tracker/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Covid Tracker</h1>
+      <ul>
+        <li>Global</li>
+        <li>Indonesia</li>
+        <li>Provins</li>
+      </ul>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
