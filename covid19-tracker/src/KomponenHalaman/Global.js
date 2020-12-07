4042f119c90e7d9a15e1e59559b26f596e2391d1
chelseawowor <s21810201@student.unklab.ac.id>
2020-12-07 21:34:24 +0800

    sixth commit -  add component provinsi finish

--- covid19-tracker/src/KomponenHalaman/Global.js
+++ covid19-tracker/src/KomponenHalaman/Global.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Card from './Card';
 import '../App.css';
+import NumberFormat from 'react-number-format';
 
 const Global = ({name, value}) => {
     const [confirmed, setConfirmed] = useState([]);
--- covid19-tracker/src/KomponenHalaman/Provinsi.js
+++ covid19-tracker/src/KomponenHalaman/Provinsi.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import '../App.css';
+import NumberFormat from 'react-number-format';
 
-const Provinsi = () => {
+const Provinsi = ({provinsi, posotif, sembuh, meninggal}) => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://indonesia-covid-19.mathdro.id/api/provinsi")
+        .then((response) => setData(response.data.data));
+    }, []);
+
+    console.log(data);
+    
     return (
         <div>
-            <h2>Data jumlah kasus di Provinsi-provinsi</h2>
+            <h2>Data jumlah kasus di Indonesia</h2>
+            <tr className="table">
+                <th>Provinsi</th>
+                <th>Jumlah Kasus</th>
+                <th>Sembuh</th>
+                <th>Meninggal</th>
+            </tr>
+            {data.map((item) => {
+                  return(
+                      <tr className="table">
+                          <th className="kolom">{item.provinsi}</th>
+                          <th className="kolom"><NumberFormat value={item.kasusPosi} /></th>
+                          <th className="kolom"><NumberFormat value={item.kasusSemb} /></th>
+                          <th className="kolom"><NumberFormat value={item.kasusMeni} /></th>
+                       </tr>
+                  )
+        })}
         </div>
     )
-}
+};
 
 export default Provinsi;
